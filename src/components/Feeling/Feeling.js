b11ca2357f5d3569d91f1d1c330f44053650d47a
Julie Zembik <kurokurosan@gmail.com>
2019-01-27 13:24:14 -0600

    started linking reducers into index starting with feeling, input returns as NaN in logger

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -5,15 +5,37 @@ import './Feeling.css';
 import { connect } from 'react-redux';
 
 class Feeling extends Component {
+    constructor () {
+        super();
+        this.state = {
+            feeling: '',
+        }
+    }
+    //stores feeling rating as local state to be sent to index.js
+    submitFeedback = (number) => {
+        this.setState({
+            feeling: Number(number)
+        })
+
+    }
+
+    submitToRedux = (event) => {
+        event.preventDefault();
+        this.props.dispatch({ type: 'RECEIVE_FEELING', payload: this.state })
+
+    }
+
+
+
     render() {
         return (
             <div className="container">
-                <div>
+                <form onSubmit={this.submitToRedux}>
                     <h2>How are you feeling today?</h2>
                     <h5>Rate your feelings from 1-5. 1 for really bad, and 5 for excellent!</h5>
                     <TextField required type="number" className="input-one" />
-                    <Button type="submit" variant="contained" color="primary" >Next</Button>
-                </div>
+                    <Button type="submit" variant="contained" color="primary" onClick={this.submitFeedback} >Next</Button>
+                </form>
             </div>
 
         );
--- src/index.js
+++ src/index.js
@@ -3,8 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
+//reducers go here
+const feedbackStart = { feeling: null };
+const receivedFeedback = ( state = feedbackStart, action ) => {
+    switch ( action.type ) {
+        case 'RECEIVE_FEELING': 
+            return { ...state, ...action.payload }
+
+        default:
+            break;
+    }
+    return state;
+}
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//end reducers
+
+const storeInstance = createStore(
+    combineReducers({
+        receivedFeedback
+    }),
+    applyMiddleware(logger),
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
