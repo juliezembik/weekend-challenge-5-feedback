ae7376af34ba205ece7bb104ab29032c1a1a4de5
Julie Zembik <kurokurosan@gmail.com>
2019-01-27 13:59:16 -0600

    finally able to send input value to show up as number and not as null or NaN

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -8,13 +8,13 @@ class Feeling extends Component {
     constructor () {
         super();
         this.state = {
-            feeling: '',
+            feeling: 0,
         }
     }
     //stores feeling rating as local state to be sent to index.js
-    submitFeedback = (number) => {
+    handleChangeState = (event) => {
         this.setState({
-            feeling: Number(number)
+            feeling: Number(event.target.value),
         })
 
     }
@@ -22,7 +22,7 @@ class Feeling extends Component {
     submitToRedux = (event) => {
         event.preventDefault();
         this.props.dispatch({ type: 'RECEIVE_FEELING', payload: this.state })
-
+        
     }
 
 
@@ -30,11 +30,12 @@ class Feeling extends Component {
     render() {
         return (
             <div className="container">
+                <h3>This is feeling number: {JSON.stringify(this.state.feeling)}</h3>
                 <form onSubmit={this.submitToRedux}>
                     <h2>How are you feeling today?</h2>
                     <h5>Rate your feelings from 1-5. 1 for really bad, and 5 for excellent!</h5>
-                    <TextField required type="number" className="input-one" />
-                    <Button type="submit" variant="contained" color="primary" onClick={this.submitFeedback} >Next</Button>
+                    <TextField required type="number" className="input-one" onChange={this.handleChangeState} />
+                    <Button type="submit" variant="contained" color="primary" >Next</Button>
                 </form>
             </div>
 
